2a1e8884bdb8e366f7019cc6710f970e7dd680b2
Victor <victorhscampos@gmail.com>
2022-04-19 15:02:23 -0300

    update

--- backend/routes/read.js
+++ backend/routes/read.js
@@ -3,11 +3,12 @@ const queries = require("../modules/queries.js");
 const router = require("express").Router();
 
 router.get("/read", async (req, res) => {
-  const usuarios = await dbConnection.query(queries.getAllUserData);
+  const usuarios = await dbConnection.query('SELECT * FROM clientes');
 
   console.log(usuarios);
 
   res.json(usuarios);
+
 });
 
 module.exports = router;
--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.use("/", express.static(""));
+app.use("/", express.static(__dirname + "/src/"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
