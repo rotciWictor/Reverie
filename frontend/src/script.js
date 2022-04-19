c833d60b31be797ab4fe69153365cb13b078fb0a
Victor <victorhscampos@gmail.com>
2022-04-19 17:06:45 -0300

    finalizando front

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,26 +1,33 @@
- $("#homeBtn").click(function() {
+//Botões de transição de page//
+$("#homeBtn").click(function () {
 
-     $("#homepage").fadeIn()
-
-     $("#registerPG").fadeOut()
-     $("#loginPG").fadeOut()
-     $("#profilePG").fadeOut()
-     $("#cartPG").fadeOut()
-     $("#catalogue").fadeOut()
- })
-
- $("#mainHeaderLogo").click(function() {
     $("#homepage").fadeIn()
 
     $("#registerPG").fadeOut()
     $("#loginPG").fadeOut()
     $("#profilePG").fadeOut()
     $("#cartPG").fadeOut()
+    $("#productPG").fadeOut()
     $("#catalogue").fadeOut()
+    $("#contactPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+})
 
- })
+$("#mainHeaderLogo").click(function () {
+    $("#homepage").fadeIn()
 
- $("#catalogueBtn").click(function() {
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#productPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#contactPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+
+})
+
+$("#catalogueBtn").click(function () {
 
     $("#catalogue").fadeIn()
 
@@ -29,11 +36,106 @@
     $("#loginPG").fadeOut()
     $("#profilePG").fadeOut()
     $("#cartPG").fadeOut()
-    
+    $("#productPG").fadeOut()
+    $("#contactPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+
 })
 
-$("#contactBtn").click(function() {
-     
-    
+$("#contactBtn").click(function () {
+
+    $("#contactPG").fadeIn()
+
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#productPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+
 })
+
+$("#product1").click(function () {
+
+    $("#productPG").fadeIn()
+    $("#hackerProductPG").css("display", "flex")
+
+    $("#runnerProductPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#contactPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+})
+
+$("#product2").click(function () {
+
+    $("#productPG").fadeIn()
+    $("#runnerProductPG").css("display", "flex")
+
+    $("#hackerProductPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#contactPG").fadeOut()
+    $("#aboutUsPG").fadeOut()
+})
+
+$("#aboutUsBtn").click(function () {
+
+    $("#aboutUsPG").fadeIn()
+
+
+    $("#productPG").fadeOut()
+    $("#hackerProductPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#contactPG").fadeOut()
+
+})
+
+$("#profileLogo").click(function () {
+
+    $("#loginPG").fadeIn()
+
+    $("#aboutUsPG").fadeOut()
+    $("#productPG").fadeOut()
+    $("#hackerProductPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#contactPG").fadeOut()
+
+})
+
+$("#loginButtonSubtext").click(function () {
+
+    $("#registerPG").fadeIn()
+
+    $("#aboutUsPG").fadeOut()
+    $("#productPG").fadeOut()
+    $("#hackerProductPG").fadeOut()
+    $("#catalogue").fadeOut()
+    $("#homepage").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#contactPG").fadeOut()
+
+})
+
 
