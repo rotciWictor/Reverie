8d97630ecfff5f8448665e1e081213eacf44211a
Victor <victorhscampos@gmail.com>
2022-04-19 14:43:04 -0300

    front

---
+++ frontend/src/script.js
@@ -0,0 +1,39 @@
+ $("#homeBtn").click(function() {
+
+     $("#homepage").fadeIn()
+
+     $("#registerPG").fadeOut()
+     $("#loginPG").fadeOut()
+     $("#profilePG").fadeOut()
+     $("#cartPG").fadeOut()
+     $("#catalogue").fadeOut()
+ })
+
+ $("#mainHeaderLogo").click(function() {
+    $("#homepage").fadeIn()
+
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    $("#catalogue").fadeOut()
+
+ })
+
+ $("#catalogueBtn").click(function() {
+
+    $("#catalogue").fadeIn()
+
+    $("#homepage").fadeOut()
+    $("#registerPG").fadeOut()
+    $("#loginPG").fadeOut()
+    $("#profilePG").fadeOut()
+    $("#cartPG").fadeOut()
+    
+})
+
+$("#contactBtn").click(function() {
+     
+    
+})
+
