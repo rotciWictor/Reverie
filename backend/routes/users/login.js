0b106d71573afbe0d1772ea72fd101c14280218b
Victor Campos <victorhscampos@gmail.com>
2022-04-28 17:20:53 -0300

    cookie aparece!!

--- backend/routes/users/login.js
+++ backend/routes/users/login.js
@@ -50,19 +50,25 @@ router.post("/", async (req, res) => {
     );
     const hashPassword = user[0].password;
 
-    console.log(user)
+    // console.log(user)
 
 
-    const matchs = await bcrypt.compare(hashPassword,password);
+    const matchs = await bcrypt.compare(password,hashPassword);
+  
+ 
 
     if (!matchs) {
       return res.status(400).json("Senha incorreta");
     }
-
-    let tokens = JWTTokens(user);
-
-    res.cookie("Cookie", tokens.refreshToken, {
-      semiSite: "none",
+    const data = {};
+    data.id = user[0].id;
+    data.name = user[0].name;
+    data.email = user[0].email;
+    console.log(data);
+    let tokens = JWTTokens(data);
+      console.log(tokens)
+    res.cookie("Cookie", tokens.accessToken, {
+      sameSite: "none",
       secure: true,
     });
     res.status(200);
--- backend/utils/jwt-helpers.js
+++ backend/utils/jwt-helpers.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
 function jwtTokens({ id, name, email }) {
   const user = { id, name, email}; 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '10m' });
+  const accessToken = jwt.sign(user, "process.env.ACCESS_TOKEN_SECRET", { expiresIn: '10s' });
+  const refreshToken = jwt.sign(user, "process.env.REFRESH_TOKEN_SECRET", { expiresIn: '10m' });
   return ({ accessToken, refreshToken });
 }
 
